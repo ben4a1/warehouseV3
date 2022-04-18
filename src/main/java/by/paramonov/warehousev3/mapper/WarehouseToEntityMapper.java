0dc06eface806c270fac06c5012b1ba54b959081
Alexey Paramonov <paramonov_tama@mail.ru>
2022-04-18 18:18:14 +0300

    fix: pom.xml - add mapstruct; warehouseMapper

---
+++ src/main/java/by/paramonov/warehousev3/mapper/WarehouseToEntityMapper.java
@@ -0,0 +1,11 @@
+package by.paramonov.warehousev3.mapper;
+
+import by.paramonov.warehousev3.entity.WarehouseEntity;
+import by.paramonov.warehousev3.model.Warehouse;
+import org.mapstruct.Mapper;
+
+@Mapper(componentModel = "spring")
+public interface WarehouseToEntityMapper {
+    WarehouseEntity warehouseToWarehouseEntity(Warehouse warehouse);
+    Warehouse warehouseEntityToWarehouse(WarehouseEntity warehouseEntity);
+}
--- src/main/java/by/paramonov/warehousev3/service/impl/WarehouseServiceImpl.java
+++ src/main/java/by/paramonov/warehousev3/service/impl/WarehouseServiceImpl.java
@@ -12,6 +12,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
+//Service layer
 @Service
 @RequiredArgsConstructor
 public class WarehouseServiceImpl implements WarehouseService {
