435c45bad59140b084d47174f071f63408d70edc
Alexey Paramonov <paramonov_tama@mail.ru>
2022-04-18 15:51:07 +0300

    initialize project v3

---
+++ src/main/java/by/paramonov/warehousev3/WarehouseV3Application.java
@@ -0,0 +1,13 @@
+package by.paramonov.warehousev3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WarehouseV3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WarehouseV3Application.class, args);
+    }
+
+}
