32300b266c31bf2e92b714fc419cab7b788f0c0e
Alexey Paramonov <paramonov_tama@mail.ru>
2022-04-18 17:23:59 +0300

    add: service interface

---
+++ src/main/java/by/paramonov/warehousev3/service/WarehouseService.java
@@ -0,0 +1,14 @@
+package by.paramonov.warehousev3.service;
+
+import by.paramonov.warehousev3.model.Warehouse;
+
+import java.util.List;
+import java.util.UUID;
+
+public interface WarehouseService {
+    // getting warehouse by id
+    Warehouse getWarehouseById(UUID id);
+
+    List<Warehouse> getAllWarehouses();
+    void addWarehouse(Warehouse warehouse);
+}
