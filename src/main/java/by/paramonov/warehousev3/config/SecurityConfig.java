a8ec3535b4b9e60a512c47a49626f931228f2d20
Alexey Paramonov <paramonov_tama@mail.ru>
2022-04-20 15:24:10 +0300

    add: SecurityConfig, start of UserDetailsService

---
+++ src/main/java/by/paramonov/warehousev3/config/SecurityConfig.java
@@ -0,0 +1,23 @@
+package by.paramonov.warehousev3.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final UserDetailsService userDetailsService;
+
+    public SecurityConfig(UserDetailsService userDetailsService) {
+        this.userDetailsService = userDetailsService;
+    }
+
+    public PasswordEncoder encoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
