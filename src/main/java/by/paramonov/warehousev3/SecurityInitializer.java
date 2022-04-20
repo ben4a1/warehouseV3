c05ac7200eb3fd85ed02f6e0dfe3b7e5856e43d3
Alexey Paramonov <paramonov_tama@mail.ru>
2022-04-20 15:12:44 +0300

    add: spring security dependencies, SecurityInitializer

---
+++ src/main/java/by/paramonov/warehousev3/SecurityInitializer.java
@@ -0,0 +1,6 @@
+package by.paramonov.warehousev3;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SecurityInitializer extends AbstractSecurityWebApplicationInitializer {
+}
