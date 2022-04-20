8d3174425f4d3bd7cded443613d54b93d037e893
Alexey Paramonov <paramonov_tama@mail.ru>
2022-04-20 12:10:18 +0300

    feat: add metod in WarehouseRepository

--- src/main/java/by/paramonov/warehousev3/repository/WarehouseRepository.java
+++ src/main/java/by/paramonov/warehousev3/repository/WarehouseRepository.java
@@ -3,7 +3,9 @@ package by.paramonov.warehousev3.repository;
 import by.paramonov.warehousev3.entity.WarehouseEntity;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface WarehouseRepository extends CrudRepository<WarehouseEntity, UUID> {
+    List<WarehouseEntity> findAllWarehousesByOwner(String owner);
 }
