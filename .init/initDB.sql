64657fe81242fc7f4a0e67c32b27079f2a2afee8
Alexey Paramonov <paramonov_tama@mail.ru>
2022-04-20 16:41:17 +0300

    add: RoleEntity

--- .init/initDB.sql
+++ .init/initDB.sql
@@ -1,6 +1,6 @@
 CREATE TABLE IF NOT EXISTS warehouse
 (
-    id    UUID PRIMARY KEY ,
+    id    BIGINT PRIMARY KEY ,
     name  VARCHAR(200) NOT NULL ,
     email VARCHAR(254) NOT NULL ,
     phone VARCHAR(20)  NOT NULL
---
+++ src/main/java/by/paramonov/warehousev3/entity/RoleEntity.java
@@ -0,0 +1,21 @@
+package by.paramonov.warehousev3.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.AttributeOverride;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import javax.websocket.server.ServerEndpoint;
+
+@Entity
+@Table(name = "role")
+@Getter
+@Setter
+@AttributeOverride(name = "id", column = @Column(name = "role_id"))
+public class RoleEntity extends BaseEntity{
+
+    @Column(name = "name")
+    private String name;
+}
