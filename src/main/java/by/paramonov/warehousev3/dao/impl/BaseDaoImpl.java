2c5d64eca99811c2a5cf753323157ab87ade0344
Alexey Paramonov <paramonov_tama@mail.ru>
2022-04-26 12:26:55 +0300

    add: BaseDaoImpl class

---
+++ src/main/java/by/paramonov/warehousev3/dao/impl/BaseDaoImpl.java
@@ -0,0 +1,46 @@
+package by.paramonov.warehousev3.dao.impl;
+
+import by.paramonov.warehousev3.dao.BaseDao;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.GenericTypeResolver;
+
+import javax.transaction.Transactional;
+import java.util.List;
+
+public class BaseDaoImpl<T> implements BaseDao<T> {
+    @Autowired
+    private SessionFactory sessionFactory;
+
+    private final Class<T> modelClass;
+
+    @SuppressWarnings("unchecked")
+    public BaseDaoImpl() {
+        modelClass = (Class<T>) GenericTypeResolver.resolveTypeArgument(getClass(), BaseDaoImpl.class);
+    }
+
+    @Override
+    public T findOne(Long id) {
+        Session session = sessionFactory.getCurrentSession();
+        return session.get(modelClass, id);
+    }
+
+    @Override
+    public void save(T entity) {
+        Session session = sessionFactory.getCurrentSession();
+        session.save(entity);
+    }
+
+    @Override
+    @Transactional
+    public List<T> findAll() {
+        return sessionFactory.getCurrentSession()
+                .createQuery("from " + modelClass.getSimpleName(), modelClass)
+                .getResultList();
+    }
+
+    protected Session getCurrentSession() {
+        return sessionFactory.getCurrentSession();
+    }
+}
