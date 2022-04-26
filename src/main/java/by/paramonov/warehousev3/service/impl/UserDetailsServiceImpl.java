b863fe4975f7b3f9bd0007cd8154c3f69aca4ce2
Alexey Paramonov <paramonov_tama@mail.ru>
2022-04-26 22:23:57 +0300

    modified: javax.persistance dependencyy

--- src/main/java/by/paramonov/warehousev3/service/impl/UserDetailsServiceImpl.java
+++ src/main/java/by/paramonov/warehousev3/service/impl/UserDetailsServiceImpl.java
@@ -5,7 +5,6 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
-import javax.transaction.Transactional;
 
 @Service
 @Transactional
