70b04dd5f059aa5072382c6008ea4abd7a85677f
Alexey Paramonov <paramonov_tama@mail.ru>
2022-04-27 20:54:26 +0300

    add: 2 methods into UserService

--- service/src/main/java/by/paramonov/warehousev3/service/UserService.java
+++ service/src/main/java/by/paramonov/warehousev3/service/UserService.java
@@ -1,5 +1,13 @@
 package by.paramonov.warehousev3.service;
 
+import by.paramonov.warehousev3.dto.UserDto;
+import by.paramonov.warehousev3.entity.UserEntity;
+
+import javax.validation.ValidationException;
+
 public interface UserService {
+    UserDto createUser(UserDto userDto)throws ValidationException;
+
+    UserEntity findByUsername(String username);
 
 }
