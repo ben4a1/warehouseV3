60888d22ca056bfb492063e83cc15360c37d2958
Alexey Paramonov <paramonov_tama@mail.ru>
2022-05-09 15:03:03 +0300

    feat: daosource() in daoconfig

--- dal/src/main/java/by/paramonov/warehousev3/config/DaoConfig.java
+++ dal/src/main/java/by/paramonov/warehousev3/config/DaoConfig.java
@@ -1,9 +1,39 @@
 package by.paramonov.warehousev3.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.sql.DataSource;
+import java.util.Objects;
 
 @Configuration
-@PropertySource("classpath:")
+@PropertySource("classpath:database.properties")
+@EnableTransactionManagement
+@ComponentScan(basePackages = "by.paramonov.warehousev3.repository")
+@EnableJpaRepositories(basePackages = "by.paramonov.warehousev3.repository")
 public class DaoConfig {
+    private Environment environment;
+
+    @Autowired
+    public DaoConfig(Environment env) {
+        this.environment = env;
+    }
+
+    @Bean
+    public DataSource dataSource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName(Objects.requireNonNull(environment.getProperty("jdbc.driver")));
+        dataSource.setUrl(environment.getProperty("jdbc.url"));
+        dataSource.setUsername(environment.getProperty("jdbc.username"));
+        dataSource.setPassword(environment.getProperty("jdbc.password"));
+        return dataSource;
+    }
+
 }
