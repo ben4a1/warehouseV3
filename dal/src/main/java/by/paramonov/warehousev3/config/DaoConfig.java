724cce8384f00d2c1cf6ad26c20ee7230521433f
Alexey Paramonov <paramonov_tama@mail.ru>
2022-05-09 14:51:04 +0300

    feat: database settings from application.properies to database.properties

---
+++ dal/src/main/java/by/paramonov/warehousev3/config/DaoConfig.java
@@ -0,0 +1,9 @@
+package by.paramonov.warehousev3.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+@Configuration
+@PropertySource("classpath:")
+public class DaoConfig {
+}
