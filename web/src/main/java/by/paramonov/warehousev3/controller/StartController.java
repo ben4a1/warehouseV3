adddc1981b57c89d35067f2cfdf20463b3c4de70
Alexey Paramonov <paramonov_tama@mail.ru>
2022-05-22 20:58:00 +0300

    feat: start.jsp

--- web/src/main/java/by/paramonov/warehousev3/controller/StartController.java
+++ web/src/main/java/by/paramonov/warehousev3/controller/StartController.java
@@ -1,14 +1,23 @@
 package by.paramonov.warehousev3.controller;
 
+import by.paramonov.warehousev3.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class StartController {
 
-    @GetMapping("/start")
-    public String start(@RequestParam(required = false) String name) {
-        return "Hello, " + name;
+   private final UserService userService;
+
+   @Autowired
+    public StartController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping(path = "/")
+    public String startPage(Model model){
+       return "start";
     }
 }
---
+++ web/src/main/webapp/WEB-INF/jsp/start.jsp
@@ -0,0 +1,12 @@
+
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Warehouse of people</title>
+</head>
+<body>
+    <div style="text-align: center">
+        Hello, my little pony!
+    </div>
+</body>
+</html>
